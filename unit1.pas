8c6558f410e9a9ddb6acc9c8238a9f42379bf6e3
Mo Hydar <hymcode@vivaldi.net>
2020-07-14 21:19:08 +0100

    Draft program layout

--- unit1.pas
+++ unit1.pas
@@ -5,10 +5,21 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
   private
 
   public
